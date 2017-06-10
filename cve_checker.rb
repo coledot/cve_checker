830a14dfab147239750b580ac4ffdf4da09434c5
Cole Thompson <cole.thompson@gmail.com>
2017-06-10 12:43:02 -0500

    initial commit

---
+++ cve_checker.rb
@@ -0,0 +1,9 @@
+#! /usr/bin/env ruby
+
+# TODO
+# - Find out what was the last vulnerability it found last time it was run
+# - Check the website for any new vulnerabilities since the last check
+# - Send an email to a particular email address with all the new vulnerabilities found. All the vulnerabilities should be sent in the same email
+# - Update it's storage of the last vulnerability found so that it doesn't email us again until a new vulnerability is published
+
+
