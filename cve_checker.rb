b910bcc4a07adc85b8dff9009e9eaa7cf459c069
Cole Thompson <cole.thompson@gmail.com>
2017-06-10 13:35:52 -0500

    email now includes new cves since last run

--- cve_checker.rb
+++ cve_checker.rb
@@ -1,9 +1,5 @@
 #! /usr/bin/env ruby
 
-# TODO
-# - Send an email to a particular email address with all the new vulnerabilities found. All the vulnerabilities should be sent in the same email
-# - Update it's storage of the last vulnerability found so that it doesn't email us again until a new vulnerability is published
-
 require 'nokogiri'
 require 'open-uri'
 require 'yaml'
@@ -23,9 +19,13 @@ cve_search_results = search_page.css('.searchresults tr.srrowns')
 
 latest_cve_info = YAML.load_file "latest_cve.yml"
 
+new_product_cves = {}
+
 if cve_search_results.length > 0
   latest_cve_result = cve_search_results[0]
   if latest_cve_result.css('td')[1].text != latest_cve_info[product_name][:cve_id]
+    new_product_cves[product_name] = []
+
     cve_search_results.each do |cve_result|
       cve_id = cve_result.css('td')[1].text
       cve_href = cve_result.css('td a')[1]['href']
@@ -34,7 +34,7 @@ if cve_search_results.length > 0
         break
       end
 
-      puts cve_id, cve_base_uri + cve_href
+      new_product_cves[product_name] << { cve_id: cve_id, cve_url: cve_base_uri + cve_href }
     end
 
     latest_cve_info = {}
@@ -43,20 +43,37 @@ if cve_search_results.length > 0
     latest_cve_info[product_name][:last_time_run] = Time.now.utc
     File.open("latest_cve.yml", "w") { |file| file.write YAML.dump latest_cve_info }
 
-    smtp_options = { :address              => "smtp.gmail.com",
-                     :port                 => 587,
-                     :user_name            => gmail_address,
-                     :password             => gmail_password,
-                     :authentication       => 'plain',
-                     :enable_starttls_auto => true  }
-    Mail.defaults do
-      delivery_method :smtp, smtp_options
+    # build message
+    message = ""
+    new_product_cves.each do |product_cve|
+      if product_cve[1].length == 0
+        next
+      end
+
+      message += "New CVEs for #{product_cve[0]}:\n"
+      product_cve[1].each do |cve|
+        message += "#{cve[:cve_id]} #{cve[:cve_url]}\n"
+      end
+      message += "\n\n"
     end
-    Mail.deliver do
-      to      "cole.thompson@gmail.com"
-      from    "cole.thompson@gmail.com"
-      subject "New CVEs found"
-      body    "test"
+
+    if message != ""
+      # send email
+      smtp_options = { :address              => "smtp.gmail.com",
+                       :port                 => 587,
+                       :user_name            => gmail_address,
+                       :password             => gmail_password,
+                       :authentication       => 'plain',
+                       :enable_starttls_auto => true  }
+      Mail.defaults do
+        delivery_method :smtp, smtp_options
+      end
+      Mail.deliver do
+        to      "cole.thompson@gmail.com"
+        from    "cole.thompson@gmail.com"
+        subject "New CVEs found"
+        body    message
+      end
     end
   end
 end
